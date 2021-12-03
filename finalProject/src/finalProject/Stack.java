2df9e29bedb1c8936bf50ddf9b74f667d7100516
Justin-Homme <jhh55998@bethel.edu>
2021-12-02 21:38:57 -0600

    Stack.java

---
+++ finalProject/src/finalProject/Stack.java
@@ -0,0 +1,70 @@
+package finalProject;
+
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+
+    /** The top node in our stack */
+    ListNode topn;
+    /** The bottom node in our stack */
+    ListNode bottom;
+
+    public Stack() {
+        topn = bottom = new ListNode(null, null);
+    } // constructor
+
+    public boolean isEmpty() {
+        if (topn == bottom) {
+            return true;
+        }
+        return false;
+    } // isEmpty
+
+    /**
+     * @param element the element that we want to add to the stack
+     */
+    public void pushTask(T element) {
+        ListNode newNode = new ListNode(element, topn);
+        topn = newNode;
+    } // push Task
+
+    /**
+     * @return the data held in the top node in the stack
+     * @throws EmptyStackException if the stack is empty
+     */
+    public T popTask() throws EmptyStackException {
+        T holder;
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        holder = topn.data;
+        topn = topn.next;
+        return holder;
+    } // popTask
+
+    /**
+     * @return the data held in the top node of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
+    public T topTask() throws EmptyStackException {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return topn.data;
+    } // topTask
+
+    private class ListNode {
+        
+        /** the data that the ListNode holds */
+        T data;
+        /** the ListNode to which this ListNode points */
+        ListNode next;
+
+        public ListNode(T data, ListNode next) {
+            this.data = data;
+            this.next = next;
+        } // constructor
+
+    } // ListNode
+
+} // Stack
