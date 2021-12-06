93066617fd9226246781de5feae23b4babe903dc
AnnaGarf <ang93332@bethel.edu>
2021-12-06 15:14:03 -0600

    Update DSStack.java
    comment

--- finalProject/src/finalProject/DSStack.java
+++ finalProject/src/finalProject/DSStack.java
@@ -8,26 +8,26 @@ public interface DSStack<T> {
 	 * @return true if stack is empty, false otherwise
 	 */
 	public boolean isEmpty();
-	
+
 	/**
 	 * 
 	 * @param element element to be added to top of stack
 	 * @throws RuntimeException if stack is already full
 	 */
 	public void push(T element) throws RuntimeException;
-	
+
 	/**
 	 * 
 	 * @return top element of stack, which is then removed from stack
 	 * @throws EmptyStackException if stack is empty
 	 */
 	public T pop() throws EmptyStackException;
-	
+	// HI
+
 	/**
 	 * 
 	 * @return top element of stack, which is not removed from stack
 	 * @throws EmptyStackException if stack is empty
 	 */
-	public T top()throws EmptyStackException;
+	public T top() throws EmptyStackException;
 }
-
