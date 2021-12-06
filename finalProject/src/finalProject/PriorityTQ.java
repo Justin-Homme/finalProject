b726f328af1385cd22379244a81a0bc664a6ea86
AnnaGarf <ang93332@bethel.edu>
2021-12-06 16:00:43 -0600

    Update PriorityTQ.java
    delete findMin

--- finalProject/src/finalProject/PriorityTQ.java
+++ finalProject/src/finalProject/PriorityTQ.java
@@ -1,9 +1,9 @@
 package finalProject;
 
 /**
- * A class for the first part of our Command Center
- * This is the priority task queue that will use a 
- * minHeap 
+ * A class for the first part of our Command Center This is the priority task
+ * queue that will use a minHeap
+ * 
  * @author Justin Homme
  * @version 12/2/21
  *
@@ -12,27 +12,18 @@ public class PriorityTQ<T> {
 
 	/** The current highest priority task */
 	private T minTask;
-	
+
 	public PriorityTQ() {
-		
+
 	} // Constructor
-	
+
 	/**
-	 * @return the highest priority task
-	 */
-	public T findMin() {
-		//TODO finish this method
-		return this.minTask;
-	} // findMin
-	
-	/**
-	 * @param task the task to be added to the queue
-	 * @param priority an int signifying the priority of the task 
-	 * (1 is highest priority, 100 is lowest priority)
+	 * @param task     the task to be added to the queue
+	 * @param priority an int signifying the priority of the task (1 is highest
+	 *                 priority, 100 is lowest priority)
 	 */
 	public void addTask(T task, int priority) {
-		//TODO finish this method
+		// TODO finish this method
 	} // addTask
-	
-	
+
 }
