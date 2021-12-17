43838c44506ced3dfae5a234d91b8e0cdf3eac36
AnnaGarf <ang93332@bethel.edu>
2021-12-17 01:57:38 -0600

    Circ node getters

--- finalProject/src/finalProject/View.java
+++ finalProject/src/finalProject/View.java
@@ -84,7 +84,6 @@ public class View extends Application {
 				myMainPane.getThirdCol().getTextTF().setText("");
 			});
 
-
 			myMainPane.getThirdCol().getRemoveTaskB().setOnAction(event -> { // third
 				Stack<String> stack = new Stack<>();
 				String myStr = myMainPane.getThirdCol().getTextTF().getText();
@@ -164,8 +163,12 @@ public class View extends Application {
 		myGP.getMyTA().setText(str);
 	} // updateGPTA
 
+	CircDLL<String> currentDisplay;
+
 	private void rotateMsg() {
 		myMainPane.getSecondCol().getMyTA();
+		currentDisplay.getCurData();
+		currentDisplay.getNxt();
 	}
 
 	public class Reminder {
